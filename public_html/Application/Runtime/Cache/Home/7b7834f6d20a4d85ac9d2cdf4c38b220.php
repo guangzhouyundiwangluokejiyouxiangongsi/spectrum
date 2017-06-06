fbbdd121b35b046b4fcce0190675bdf6475d6bc1
陆桂文 <aa208349@163.com>
2017-06-06 13:34:41 +0800

    删除public_html

--- public_html/Application/Runtime/Cache/Home/7b7834f6d20a4d85ac9d2cdf4c38b220.php
+++
@@ -1,7 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit(); if(is_array($company)): foreach($company as $k=>$com): ?><div class="c_list">
-		            <a target="_blank" href="/CompanyInfo/details?id=<?php echo ($com["com_id"]); ?>">
-		                <img src="<?php echo tempimg($com['img_id'],370,200);?>" >
-		                <h4><?php echo ($com['com_name']); ?></h4>
-		                <p><?php echo ($com['com_synopsis']); ?></p>
-		            </a>
-		        </div><?php endforeach; endif; ?>
